4d67971575d75bf35e9144bf74fd368a0c7f4cf1
yummygyudon <bang2brew@gmail.com>
2023-05-21 02:13:20 +0900

    [FEAT] 도메인 Repository 생성

---
+++ SOPKATHON7-SERVER/SopkathonServer/src/main/java/com/sopt/SopkathonServer/api/comment/repository/CommentRepository.java
@@ -0,0 +1,8 @@
+package com.sopt.SopkathonServer.api.comment.repository;
+
+import com.sopt.SopkathonServer.api.comment.domain.Comment;
+import org.springframework.data.repository.Repository;
+
+public interface CommentRepository extends Repository<Comment, Long> {
+
+}
---
+++ SOPKATHON7-SERVER/SopkathonServer/src/main/java/com/sopt/SopkathonServer/api/station/repository/StationRepository.java
@@ -0,0 +1,8 @@
+package com.sopt.SopkathonServer.api.station.repository;
+
+import com.sopt.SopkathonServer.api.station.domain.Station;
+import org.springframework.data.repository.Repository;
+
+public interface StationRepository extends Repository<Station, Long> {
+
+}
